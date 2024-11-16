fe07384c071b3775186b7889ab87033c815fb20c
David Leon <davidleon123@gmail.com>
2024-11-16 18:02:00 +0100

    added load_data

--- src/loader.py
+++ src/loader.py
@@ -1,13 +1,36 @@
 from langchain_chroma import Chroma
 #from langchain_openai import OpenAIEmbeddings
-
+from langchain_community.document_loaders import PyPDFLoader
 
 persist_directory = '../data'
-
-#embedding = OpenAIEmbeddings()
 vectordb = Chroma(
     persist_directory=persist_directory,
 #    embedding_function=embedding
 )
 
-print(vectordb._collection.count())
+
+def load_data(file_path):
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+    print(docs)
+    print(docs[0].page_content[0:100])
+    return docs
+
+
+
+
+
+
+
+
+#embedding = OpenAIEmbeddings()
+
+
+
+def main():
+    file_path = "../project/irs.pdf"
+    load_data(file_path)
+    #print(vectordb._collection.count())
+
+if __name__ == "__main__":
+    main()
