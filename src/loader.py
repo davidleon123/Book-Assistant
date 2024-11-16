ffd271c12f2729b4027ce3a9f891335e6565035a
David Leon <davidleon123@gmail.com>
2024-11-16 23:26:53 +0100

    first uploading chain working

--- src/loader.py
+++ src/loader.py
@@ -1,36 +1,61 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, List
 from langchain_chroma import Chroma
-#from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv, find_dotenv
 
-persist_directory = '../data'
+import os
+
+if TYPE_CHECKING:
+    from langchain_core.documents import Document
+    
+load_dotenv(find_dotenv()) # read local .env file
+
+
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+db_name = "data"
+persist_directory = os.path.join(project_root, db_name) # where to store the database
+
+EMBEDDING = OpenAIEmbeddings()
+
 vectordb = Chroma(
     persist_directory=persist_directory,
-#    embedding_function=embedding
+    embedding_function=EMBEDDING
 )
 
 
-def load_data(file_path):
+def load_data(file_path: str)->List[Document]:
     loader = PyPDFLoader(file_path)
     docs = loader.load()
-    print(docs)
-    print(docs[0].page_content[0:100])
     return docs
 
 
+def split_data(documents: List[Document])->List[Document]:
+    text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1500,
+    chunk_overlap = 150
+    )
+    splits = text_splitter.split_documents(documents)
+    return splits
 
 
 
 
+def main()->None:
+    doc_to_load_path = os.path.join(project_root, "irs.pdf")
+    docs = load_data(doc_to_load_path)
+    print(len(docs))
+    print(docs[0].page_content[0:10])
+    splits = split_data(docs)
+    print(len(splits))
+    print(splits[0].page_content[0:10])
+    ids = vectordb.add_documents(splits)
+    print(ids)
 
-
-#embedding = OpenAIEmbeddings()
-
-
-
-def main():
-    file_path = "../project/irs.pdf"
-    load_data(file_path)
-    #print(vectordb._collection.count())
+    print(vectordb._collection.count())
 
 if __name__ == "__main__":
     main()
