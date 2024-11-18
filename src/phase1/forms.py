d752d16cee76be1d310a3f628ed223a2c298f639
Dea María Léon <deamarialeon@gmail.com>
2024-11-18 17:28:42 +0100

    prettier

--- src/phase1/forms.py
+++ src/phase1/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class QuestionForm(forms.Form):
     question = forms.CharField(
         label="",
-        max_length=500,
-        widget=forms.Textarea(attrs={"rows": 10, "cols": 50})
+        max_length=600,
+        widget=forms.Textarea(attrs={"rows": 8, "cols": 70})
     )
     
