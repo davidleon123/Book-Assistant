c1d1b61f95eda710ca222bedb2ef0d4a1c2d6d42
Dea María Léon <deamarialeon@gmail.com>
2024-11-18 14:48:40 +0100

    wip

--- src/phase1/forms.py
+++ src/phase1/forms.py
@@ -5,6 +5,6 @@ class QuestionForm(forms.Form):
     question = forms.CharField(
         label="",
         max_length=500,
-        widget=forms.Textarea(attrs={"size": "200"})
+        widget=forms.Textarea(attrs={"rows": 10, "cols": 50})
     )
     
