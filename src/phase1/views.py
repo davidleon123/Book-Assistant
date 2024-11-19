82693d67f45398269d4f642700e36d392679d99c
Dea María Léon <deamarialeon@gmail.com>
2024-11-19 16:15:33 +0100

    using answers

--- src/phase1/views.py
+++ src/phase1/views.py
@@ -2,26 +2,18 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
+import db_handler as db
+
 from .forms import QuestionForm
-
-#def index(request) -> HttpResponse:
-#    return render(request, 'phase1/index.html')
-
-
-def helper():
-    return "B"
-
-
-def home(request):
-    
-    context = {"a": helper()}
-    return render(request, "phase1/home.html", context)
 
 
 def generate_answer(question):
-    # Your logic to generate an answer based on the question
-    answer = f"Answer to: {question}"
-    return answer + "XYZ"
+    #answer = f"Answer to: {question}"
+    #answer = db.answer_question(question)
+    answer = db.load_answer()
+    return db.format_answer(answer)
+    #return answer
+
 
 def index(request):
     if request.method == 'POST':
@@ -31,7 +23,7 @@ def index(request):
         if form.is_valid():
             question = form.cleaned_data['question']
             answer = generate_answer(question)
-            # You can pass the answer to the context if needed
+            # pass the answer to the context
             return render(request, 'phase1/index.html', {'form': form, 'answer': answer})
     else:
         form = QuestionForm()
